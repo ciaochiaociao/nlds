137f07bddf25482c9be9ab1ba4cc63bcf71feb6f
Chiao-Wei Hsu <ciaochiaociao@gmail.com>
2019-09-03 07:48:45 +0000

    Add useful functions in utils.py

--- nlu/utils.py
+++ nlu/utils.py
@@ -2,6 +2,11 @@ from typing import Callable, Iterable
 
 from ansi.color import fg
 
+import logging
+import time
+import re
+
+
 SEPARATOR = ' '
 CLEAR_SEPARATOR = '|'
 TO = '->'
@@ -52,3 +57,45 @@ def ls_to_ls_str(list1: Iterable, list2: Iterable, sep=None, to=None):
     if sep is None:
         sep = CLEAR_SEPARATOR
     return sep_str(list1, sep) + TO_SEP + sep_str(list2, sep)
+
+
+def camel_to_snake(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+def setup_logger(name, log_file, level=logging.INFO):
+    """Function setup as many loggers as you want"""
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s')
+
+    handler = logging.FileHandler(log_file)        
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+
+def logtime(logger_name):
+    def timeit(method):
+        def timed(*args, **kw):
+            ts = time.time()
+            logging.getLogger(logger_name).info("Running function {}".format(method.__name__))
+            result = method(*args, **kw)
+            te = time.time()
+            logging.getLogger(logger_name).info("function {} ran in {}s".format(method.__name__, round(te - ts, 2)))
+            return result
+        return timed
+    return timeit
+
+
+def lprint(*args, **kwargs):
+    import inspect
+    import os
+    import sys
+    callerFrame = inspect.stack()[1]  # 0 represents this line
+    myInfo = inspect.getframeinfo(callerFrame[0])
+    myFilename = os.path.basename(myInfo.filename)
+    print('{}({}):'.format(myFilename, myInfo.lineno), *args, flush=True, file=sys.stderr, **kwargs)
