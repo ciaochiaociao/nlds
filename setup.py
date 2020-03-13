c0ecad36e16fb717d7208d50a1771f5fd8b790d9
ciaochiaociao <ciaochiaociao@gmail.com>
2020-03-13 10:31:44 +0800

    Update setup.py (add nlu package directory back and update ver number)

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ner_error_analysis',
-    version='0.1',
+    version='0.2',
     # packages=['ext_utils'],
     packages=find_packages(),
-    # package_dir={'': 'nlu'},
+    package_dir={'': 'nlu'},
     url='https://github.com/ciaochiaociao/ner_error_analysis',
     license='',
     author='Chiao-Wei Hsu',
-    author_email='tony790927@gmail.com',
+    author_email='ciaochiaociao@gmail.com',
     description='NER Error Analysis for column (conll format) dataset including CoNLL-2003, WNUT-2017, ...',
     long_description=long_description,
     long_description_content_type="text/markdown",
