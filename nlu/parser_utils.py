9a09c169389d1806cefadc90ddd432b9eb1525e6
ciaochiaociao <ciaochiaociao@gmail.com>
2020-08-31 14:14:11 +0800

    Modify iob tag scheme conversion copied from flair

--- nlu/parser_utils.py
+++ nlu/parser_utils.py
@@ -105,41 +105,56 @@ def iob12iob2(tags):
     .. seealso:: `https://github.com/flairNLP/flair/blob/master/flair/data.py <https://github.com/flairNLP/flair/blob/master/flair/data.py>`_.
     """
     for i, tag in enumerate(tags):
-        if tag.value == "O":
+        if tag == "O":
+            new_tags.append(tag)
             continue
-        split = tag.value.split("-")
+
+        # validity check (the case which is so invalid that it can not be fixed)
+        split = tag.split("-")
         if len(split) != 2 or split[0] not in ["I", "B"]:
+            # (cwhsu) check 'X-X' and 'B-, I-' # FIXME: len(split) == 3 when using wnut, e.g., I-creative-work
             return False
         if split[0] == "B":
             continue
-        elif i == 0 or tags[i - 1].value == "O":  # conversion IOB1 to IOB2
-            tags[i].value = "B" + tag.value[1:]
-        elif tags[i - 1].value[1:] == tag.value[1:]:
+        elif i == 0 or tags[i - 1] == "O":  # conversion IOB1 to IOB2
+            # (cwhsu) case: 'I-X'; when the left boundary is detected (BOE, beginning of an entity)
+            new_tags.append("B" + tag[1:])
+        elif tags[i - 1][1:] == tag[1:]:
+            # (cwhsu) case: 'I-X' and is not BOE; check if the suffix of the previous tag is the same as the current tag
+            new_tags.append(tag)
             continue
         else:  # conversion IOB1 to IOB2
-            tags[i].value = "B" + tag.value[1:]
-    return True
+            # (cwhsu) case: 'I-X' + not BOE BUT has type mismatch with the previous tag of the same entity
+            # => create a new entity
+            # e.g. B-ORG I-PER => B-ORG B-PER
+            new_tags.append("B" + tag[1:])
+
+    return new_tags
 
 
 def iob22iobes(tags):
     """
     IOB -> IOBES
-    .. seealso:: `https://github.com/flairNLP/flair/blob/master/flair/data.py <https://github.com/flairNLP/flair/blob/master/flair/data.py>`_.
+    .. seealso:: modified from `https://github.com/flairNLP/flair/blob/master/flair/data.py <https://github.com/flairNLP/flair/blob/master/flair/data.py>`_.
     """
     new_tags = []
     for i, tag in enumerate(tags):
-        if tag.value == "O":
-            new_tags.append(tag.value)
-        elif tag.value.split("-")[0] == "B":
-            if i + 1 != len(tags) and tags[i + 1].value.split("-")[0] == "I":
-                new_tags.append(tag.value)
+        if tag == "O":
+            new_tags.append(tag)
+        elif tag.split("-")[0] == "B":
+            if i + 1 != len(tags) and tags[i + 1].split("-")[0] == "I":
+                # (cwhsu) not reaching the right boundary (not EOE, not End Of an Entity)
+                # note -        EOE:    EOS     OR      the next tag is not 'I' ('B-X' or 'O')
+                #           not EOE: not EOS    AND     the next tag is     'I' ('B-X' or 'O')
+                new_tags.append(tag)
             else:
-                new_tags.append(tag.value.replace("B-", "S-"))
-        elif tag.value.split("-")[0] == "I":
-            if i + 1 < len(tags) and tags[i + 1].value.split("-")[0] == "I":
-                new_tags.append(tag.value)
-            else:
-                new_tags.append(tag.value.replace("I-", "E-"))
+                # (cwhsu) convert 'B' to 'S' when it's EOE
+                new_tags.append(tag.replace("B-", "S-"))
+        elif tag.split("-")[0] == "I":
+            if i + 1 < len(tags) and tags[i + 1].split("-")[0] == "I":
+                new_tags.append(tag)
+            else:  # (cwhsu) convert 'I' to 'E' when it's EOE
+                new_tags.append(tag.replace("I-", "E-"))
         else:
             raise Exception("Invalid IOB format!")
     return new_tags
